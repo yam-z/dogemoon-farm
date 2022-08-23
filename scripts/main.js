3b8ed44622d10f8318a131af0fa0b902b98d3a7b
yam-z <111208214+yam-z@users.noreply.github.com>
2022-08-24 00:42:01 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -21,12 +21,18 @@ async function connectDapp(){
 
 const abi = await $.getJSON("./config/abi.json"); //MAKE SURE IS FARM ABI
 const dogemoonABI = await $.getJSON("./config/dogemoonABI.json");
+const LPfarmABI = await $.getJSON("./config/LPfarmABI.json");
+const approveABI = await $.getJSON("./config/approveABI.json");
 
 const CONTRACT_ADDRESS = "0x2335c4bBF89F19982a060370ab1c892b242445aa";
 const DOGEMOON_ADDY = "0x95426E416bA98bA31C1904D7Ba46d374EC4B145A";
+const LP_STAKE_ADDRESS = "0x6ed3F2C81D766f12c7eab8439aa7397D46512f59";
+const LP_TOKEN_ADDRESS = "0x01BEB42Da29C04b0D63B7D57A1849C3D81e4Ac20";
 
 const contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
 const DogeMoonContract = new web3.eth.Contract(dogemoonABI, DOGEMOON_ADDY);
+const LPstakeContract = new web3.eth.Contract(LPfarmABI, LP_STAKE_ADDRESS);
+const LPtoken = new web3.eth.Contract(approveABI, LP_TOKEN_ADDRESS);
 
 
 setInterval(async () => {
@@ -39,9 +45,9 @@ async function updateInfo(){
 
 let timeCheck = await userTimeLockCheck();
 let allowance = await checkAllowance();
+let lpAllowance = await lpAllowanceCheck();
 let farmStatus = false;
 farmStatus = await checkFarmStatus();
-console.log(farmStatus+" FARM STATUS");
 if(farmStatus == false){
   document.getElementById('openFarm_button').innerHTML = "Open Farm";
 } else {
@@ -50,6 +56,10 @@ if(farmStatus == false){
 
 if(allowance > 0){
   document.getElementById('approve_button').style.visibility = 'hidden';
+}
+
+if(lpAllowance > 0){
+  document.getElementById('approveLP_button').style.visibility = 'hidden';
 }
 
 if(owner == ethereum.selectedAddress){
@@ -67,8 +77,11 @@ if(timeCheck == true){
 }
 
 document.getElementById("rewards").innerHTML = "Your current rewards: "+await currentRewards()+" DogeMoon!";
+document.getElementById("LPrewards").innerHTML = "Your current rewards: "+await currentLPRewards()+" DogeMoon!";
 document.getElementById("userBalance").innerHTML = "Your staked DogeMoon: "+await userBalance()+" DogeMoon!";
+document.getElementById("userBalanceLP").innerHTML = "Your staked LP tokens: "+await userLPBalance()+" LP tokens!";
 document.getElementById("totalDeposits").innerHTML = "Total staked DogeMoon: "+await returnTotalStaked()+" DogeMoon!";
+document.getElementById("totalDepositsLP").innerHTML = "Total staked LP tokens: "+await totalLPStaked()+" LP tokens";
 }
 
 
@@ -79,8 +92,21 @@ async function stake(amount){
     }))
 }
 
+async function stakeLP(amount){
+  await LPstakeContract.methods.stake(amount).send({from: ethereum.selectedAddress}).on("receipt", ( () => {
+      console.log("done");
+  }))
+}
+
+
 async function unstake(){
   await contract.methods.unstakeDogeMoon().send({from: ethereum.selectedAddress}).on("receipt", ( () => {
+      console.log("done");
+  }))
+}
+
+async function unstakeLP(amount){
+  await LPstakeContract.methods.withdraw(amount).send({from: ethereum.selectedAddress}).on("receipt", ( () => {
       console.log("done");
   }))
 }
@@ -108,9 +134,19 @@ async function currentRewards(){
       return rewards.toFixed()/(10**18);
 }
 
+async function currentLPRewards(){
+  let rewards = new BigNumber(await LPstakeContract.methods.earned(ethereum.selectedAddress).call())/(10**18);
+  return rewards.toFixed(18);
+}
+
 async function checkAllowance(){
     let allowance = new BigNumber(await DogeMoonContract.methods.allowance(ethereum.selectedAddress, CONTRACT_ADDRESS).call());
     return allowance.toFixed();
+}
+
+async function lpAllowanceCheck(){
+  let allowance = new BigNumber(await LPtoken.methods.allowance(ethereum.selectedAddress, LP_STAKE_ADDRESS).call());
+  return allowance.toFixed();
 }
 
 async function userBalance(){
@@ -118,10 +154,21 @@ async function userBalance(){
       return balance/(10**18);
 }
 
+async function userLPBalance(){
+  let balance = await LPstakeContract.methods._balances(ethereum.selectedAddress).call();
+      return balance/(10**18);
+}
+
 async function totalStaked(){
   let balance = await contract.methods.balanceOf(CONTRACT_ADDRESS).call();
       return balance/(10**18);
 }
+
+async function totalLPStaked(){
+  let balance = await LPstakeContract.methods._totalSupply().call();
+      return balance/(10**18);
+}
+
 
 async function checkFarmStatus(){
   let farmStatus = await contract.methods.isFarmOpen().call();
@@ -152,6 +199,15 @@ async function approveDogemoon(){
 }));
 }
 
+async function approveLP(){
+  let maxValue = new BigNumber("115792089237316195423570985008687907853269984665640564039457584007913129639935");
+  console.log(Number(maxValue));
+  console.log(maxValue.toFixed())
+  await LPtoken.methods.approve(LP_STAKE_ADDRESS, maxValue.toFixed()).send({from: ethereum.selectedAddress}).on("receipt", ( () => {
+    console.log("done");
+}));
+}
+
 async function toggleFarm(){
   let isOpen = await checkFarmStatus();
   if(isOpen == false){
@@ -171,6 +227,11 @@ $(document).ready(function () {
     approveDogemoon();
 })});
 
+$(document).ready(function () {
+  $("#approveLP_button").click( () => {
+    approveLP();
+})});
+
 
 $(document).ready(function async () {
     $("#openFarm_button").click( () => {
@@ -182,6 +243,21 @@ $(document).ready(function async () {
     $("#unstake_button").click( () => {
         console.log("clicked unstake");
         unstake(); 
+  })});
+
+  $(document).ready(function async () {
+    $("#unstakeLP_button").click( () => {
+        console.log("clicked unstake LP");
+        let amountToUnstake = document.getElementById("unstakeAmountLP").value;
+        if(amountToUnstake > 0){
+          amountToUnstake = BigNumber(amountToUnstake*(10**18));
+          console.log(amountToUnstake.toFixed());
+          unstakeLP(amountToUnstake.toFixed());
+        }
+        else {
+          console.log("RETURNING");
+          return;
+        }
   })});
 
   $(document).ready(function async () {
@@ -203,4 +279,21 @@ $(document).ready(function async () {
           return;
         }
   })});
+
+  
+  $(document).ready(function () {
+    $("#stakeLP_button").click( () => {
+        console.log("clicked");
+        let amountToStake = document.getElementById("stakeAmountLP").value;
+        if(amountToStake > 0){
+          amountToStake = BigNumber(amountToStake*(10**18));
+          console.log(amountToStake.toFixed());
+          stakeLP(amountToStake.toFixed());
+        }
+        else {
+          console.log("returning");
+          return;
+        }
+  })});
+
 
